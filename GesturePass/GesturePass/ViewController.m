35b6c9dae01080926a69c265eb05d9acdf7fcecc
danieldrichards <daniel@moversanshakers.mobi>
2013-09-28 15:03:40 -0700

    print shake if there is a shake

--- GesturePass/GesturePass/ViewController.m
+++ GesturePass/GesturePass/ViewController.m
@@ -14,16 +14,49 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(void) viewDidLoad {
+    // Request to turn on accelerometer and begin receiving accelerometer events
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
+}
+
+- (void)orientationChanged:(NSNotification *)notification {
+    // Respond to changes in device orientation
+}
+
+-(void) viewDidDisappear {
+    // Request to stop receiving accelerometer events and turn off accelerometer
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+}
+
+//For another example of responding to UIDevice orientation changes, see the AlternateViews sample code project.
+
+
+
+
+// respond to motion
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake)
+    {
+        // User was shaking the device. Post a notification named "shake."
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"shake" object:self];
+        
+        NSLog(@"shake");
+    }
+}
+
+
+
+
 
 @end
